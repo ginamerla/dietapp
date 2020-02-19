7313eee914cf234f77faa572c1168b4a16fd1331
Victor Herrera <vampy.mcr.23@gmail.com>
2020-02-18 20:39:26 -0500

    inicio de feature shopping list incompleto

---
+++ src/main/java/com/tortu/api/daos/ShoppingIngredients.java
@@ -0,0 +1,9 @@
+package com.tortu.api.daos;
+
+import java.util.List;
+
+public interface ShoppingIngredients {
+    public static final String QUERY = "";
+
+    public List getShoppingList(Integer idUsuario);
+}
