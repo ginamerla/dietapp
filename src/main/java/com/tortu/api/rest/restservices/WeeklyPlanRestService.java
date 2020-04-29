858f14ed0788c15eb8d4197c09687b3d45340205
unknown <geormerla@gmail.com>
2020-04-29 11:52:51 -0400

    se cancela el cambio del json de resultado para el pdf

--- src/main/java/com/tortu/api/rest/restservices/WeeklyPlanRestService.java
+++ src/main/java/com/tortu/api/rest/restservices/WeeklyPlanRestService.java
@@ -5,8 +5,6 @@ import com.tortu.api.rest.resources.WeeklyPlanResource;
 import com.tortu.api.rest.validators.GenericValidator;
 import com.tortu.api.services.WeeklyPlanService;
 import com.tortu.api.utils.GeneralException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
